c74b30fc11ea463f0956c9b93228bd0d46c47f38
OTC-PR <nikhilsah236@gmail.com>
2025-12-28 19:47:37 +0530

    Java Files

---
+++ Bill.java
@@ -0,0 +1,136 @@
+import java.util.*;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class Bill
+{
+    public static void time()
+    {
+        LocalDateTime now = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss" );
+        System.out.println("Receipt Date & Time : "+now.format(formatter));
+    }
+    public static void welcome()
+    {
+        System.out.println("================================");
+        System.out.println("======== Welcome to.... ========");
+        System.out.println("========== Smart Store =========");
+        System.out.println("Address : Bistupur, Jamshedpur ");
+        System.out.println("Contact : 0657 976976 ");
+        System.out.println("GST No  : 20ABCDE1234F1Z5");
+        System.out.println("================================");
+    }
+    public static void items()
+    {
+        System.out.println("| ID | Product Name | Price     | Quantity  |\r");
+        System.out.println("| -- | ------------ | --------- | --------  |\r");
+        System.out.println("| 1  | Pen          | 10        | 50        |\r");
+        System.out.println("| 2  | Notebook     | 50        | 30        |\r");
+        System.out.println("| 3  | Pencil       | 5         | 100       |\r");
+        System.out.println("| 4  | Eraser       | 3         | 80        |\r");
+        System.out.println("| 5  | Marker       | 25        | 40        |\r");
+        System.out.println("| 6  | Scale        | 15        | 60        |\r");
+        System.out.println("| 7  | Sharpener    | 7         | 70        |\r");
+        System.out.println("| 8  | File Folder  | 30        | 20        |\r");
+        System.out.println("| 9  | Highlighter  | 40        | 30        |\r");
+        System.out.println("| 10 | Stapler      | 120       | 10        |\r");
+        System.out.println("| -- | ------------ | --------- | --------  |\r");
+
+    }
+    public static class Parcel
+    {
+    public int sum;
+    public Double tt;
+    public String arn[];
+    public int arq[];
+    public int arp[];
+    public int ap[];
+    public int q;
+    public Parcel(int sum, Double tt, String arn[], int arq[], int arp[], int ap[], int q)
+    {
+        this.sum=sum;
+        this.tt=tt;
+        this.arn=arn;
+        this.arq=arq;
+        this.arp=arp;
+        this.ap=ap;
+        this.q=q;
+
+    }
+    }
+    
+    public static Parcel qty()
+    {
+        Scanner in=new Scanner(System.in);
+        int arri[] ={1,2,3,4,5,6,7,8,9,10};
+        String arrn[]={"Pen","Notebook","Pencil","Eraser","Marker","Scale","Sharpener","File Folder","Highlighter","Stapler"};
+        int arrp[]={10,50,5,3,25,15,7,30,40,120};
+        int arrq[]={50,30,100,80,40,60,70,20,30,10};
+        String arn[]=new String[50];
+        int arq[]=new int[50];
+        int arp[]=new int[50];
+        int ap[]=new int[50];
+        int n=0;
+        System.out.println("Enter no of items");
+        int x;
+        int q=0;
+        int q1;
+        int sum=0;
+        Double tt;
+        x=in.nextInt();
+        for(int i=1;i<=x;i++)
+        {
+            System.out.println("Enter your "+(i)+" item in cart acc to product id");
+            q=in.nextInt();
+            arn[n]=arrn[q];
+            
+            System.out.println("Enter no of "+arrn[q-1]+" quantities");
+            q1=in.nextInt();
+            arq[n]=q1;
+            ap[n]=arrp[q-1];
+            arp[n]=arrp[q-1]*q1;
+            System.out.println("price of this product of "+q1+" qty is "+(arrp[q-1]*q1)+" rupees ");
+            sum+=(arrp[q-1]*q1);
+            n++;
+        }
+    
+        tt=sum+(sum*0.05);
+        System.out.println("Your total Bill Without tax is = "+sum);
+        System.out.println("Your bill with 5% GST is : "+tt);
+        return new Parcel(sum, tt, arn, arq, arp, ap, q);
+    }
+    public static void invoice()
+    {
+        Parcel result = Bill.qty();
+        System.out.println("Items in the cart");
+        for(int i=0;i<result.q;i++)
+        {
+            System.out.println(result.arn[i]+" x "+result.arq[i]+"@"+result.ap[i]+" = "+result.arp[i]);//  From here
+        }
+        System.out.println(result.sum);
+        System.out.println(result.tt);
+       
+    }
+    /*public static int qty(int x)
+    {
+        int arr[] ={1,2,3,4,5,6,7,8,9,10};
+        return arr[x];
+    }  */
+    public static void main(String[] args)
+    {
+        Scanner in =new Scanner (System.in); 
+        Bill.time();
+        Bill.invoice();
+        /*System.out.println("Enter no");
+        int x;
+        x=in.nextInt();
+        System.out.println(bill.qty(x));
+        bill.items();
+        bill.time();
+        in.nextLine();
+        bill.welcome();
+        */
+
+    }
+}
+
---
+++ time.java
@@ -0,0 +1,13 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class time
+{
+    public static void main(String[] args)
+    {
+        LocalDateTime now = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-YYYY HH:mm:ss" );
+        System.out.println("Current Date & Time : "+now.format(formatter));
+
+    }
+}
